6353d28249515b58e6b2ec1ab05df56fc3457a15
Yating <yz1339@nyu.edu>
2016-05-02 18:07:37 -0400

    fix leg bug

--- webcamTakePicture.py
+++ webcamTakePicture.py
@@ -3,14 +3,15 @@ from cv2 import *
 def takePicture():
 
 	# initialize the camera
-	cam = VideoCapture(1)   # 0 -> index of camera
+	cam = VideoCapture(0)   # 0 -> index of camera
 	s, img = cam.read()
 	if s:    # frame captured without any errors
 	    namedWindow("cam-test",CV_WINDOW_AUTOSIZE)
 	    imshow("cam-test",img)
 	    waitKey(0)
 	    destroyWindow("cam-test")
-	    # imwrite("test1.jpg",img) #save image
+	    imwrite("test1.jpg",img) #save image
 	    return img
 
 
+takePicture()
