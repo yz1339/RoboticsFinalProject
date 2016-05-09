57f68154ca770050670c11329983db410cd37828
Alexandra <alexandraserralta@yahoo.com>
2016-05-09 19:16:31 -0400

    comments removal

--- findLegs.py
+++ findLegs.py
@@ -21,10 +21,10 @@ def drawLegs(img, topTen):
 		x = (int(topTen[i][0]) + 1) * 10
 		y = (int(topTen[i][1]) + 1) * 5
 		cv2.circle(img, (x, y), 5, (225,0,0), -1)
-		# print(separation.convert(x,y), x, y)
+		
 	cv2.imshow('circle',img)
 	cv2.waitKey(0)
-	# cv2.destroyAllWindows()
+	
 
 def unionFind(topTen):
 	legs = []	
@@ -52,16 +52,14 @@ def unionFind(topTen):
 	return u_legs
 
 def findLegs():
-	# img = cv2.imread('chairLeg1.jpg')
-	# img = imresize(img,(480,640))
+	
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	lower_hue = np.array([0,0,0])
 	upper_hue = np.array([50,50,100])
-	# upper_hue = np.array([30,30,30])
+
 
 	mask = cv2.inRange(img, lower_hue, upper_hue)
-	# cv2.imshow('mask', mask)
-	# cv2.waitKey(0)
+
 	lines = cv2.HoughLinesP(mask, 1, np.pi, 75, None, 13, 1);
 	voteM = np.zeros((128,196))
 	if lines is None:
@@ -71,12 +69,10 @@ def findLegs():
 	    if angle != 0:
 	    		cv2.line(img,(x1,y1),(x2,y2),(0,255,0),1)
 
-	    		# print(x1,y1)
+	    		
 	    		if y1 > 275:	
 	    			voteM = vote(x1, y1, voteM)
-	#cv2.imshow('hough', img)
-	#cv2.waitKey(0)
-	# get top 10 votes
+
 	topTen = []
 	topCX = np.zeros(10)
 	topCY = np.zeros(10)
@@ -90,9 +86,7 @@ def findLegs():
 					maxVote = voteM[x][y]
 					maxX = x
 					maxY = y
-		# topTen[i] = maxVote
-		# topCX[i] = maxX
-		# topCY[i] = maxY
+	
 		topTen.append((maxX,maxY))
 		voteM[maxX][maxY] = 0
 	topTen.sort()
@@ -102,18 +96,10 @@ def findLegs():
 
 	test = unionFind(topTen)
 
-	print('number of legs')
-	print(len(test))
 
 	return test
-	# print(topCX)
-	# print(topCY)
 
 
-# legs = findLegs()
-# print(legs)
-# for i in range(0,len(legs)):
-# 	print(separation.convert(legs[i][0]*10, legs[i][1]*5))
 
 
 
